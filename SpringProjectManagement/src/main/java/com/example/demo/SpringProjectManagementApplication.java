6c47e7a269b5b6f54035492b9bb0267fed975245
Spoorthi G <spoorthiggowda917@gmai.com>
2025-12-10 11:51:12 +0530

    First commit

---
+++ SpringProjectManagement/src/main/java/com/example/demo/SpringProjectManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectManagementApplication.class, args);
+		System.out.println("Application Loaded Successfully");
+	}
+
+}
